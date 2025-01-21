2211b188e909ab34e7ac32320001d085fe4160aa
fractal-fusion <fractal.fusion@27188@gmail.com>
2025-01-21 17:55:00 -0500

    actually improved heading gain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -81,11 +81,11 @@ public final class MecanumDrive {
         // path controller gains
         public double axialGain = 5.0;
         public double lateralGain = 1.0;
-        public double headingGain = 0.8; // shared with turn
+        public double headingGain = 2.0; // shared with turn
 
         public double axialVelGain = 1.0;
         public double lateralVelGain = 0.0;
-        public double headingVelGain = 0.0; // shared with turn
+        public double headingVelGain = 1.0; // shared with turn
     }
 
     public static Params PARAMS = new Params();
